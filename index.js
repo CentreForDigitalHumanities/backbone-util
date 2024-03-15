012defa564399df172b05c5348783c2173392e37
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-15 21:42:08 +0100

    Add trailing slash to the index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as wrapWithCSRF } from './src/csrf.js';
 export * from './src/future-attribute.js';
 export { default as mixin } from './src/mixin.js';
+export * from './src/trailing-slash.js';
