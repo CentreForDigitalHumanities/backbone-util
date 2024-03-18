4cdf2167fac1b4e90a0a9048f3db138b7919b5f3
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-18 17:01:14 +0100

    Add alt-click debug tool to index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export { default as getAltClickDebugMethods } from './src/click-to-debug.js';
 export { default as wrapWithCSRF } from './src/csrf.js';
 export * from './src/future-attribute.js';
 export { default as mixin } from './src/mixin.js';
