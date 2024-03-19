09a5e9df542f9814a48505f8353a46e6c5121239
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-19 14:49:58 +0100

    Add internal link enabler to the index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export { default as getAltClickDebugMethods } from './src/click-to-debug.js';
 export { default as wrapWithCSRF } from './src/csrf.js';
 export * from './src/future-attribute.js';
+export { default as makeLinkEnabler } from './src/internal-links.js';
 export { default as mixin } from './src/mixin.js';
 export * from './src/trailing-slash.js';
