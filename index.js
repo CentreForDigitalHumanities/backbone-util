39aaf4f86d2aea94ad6635b1c4b68b5484a9ebca
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-19 23:37:07 +0100

    Add the user model mixin to the index

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export * from './src/future-attribute.js';
 export { default as makeLinkEnabler } from './src/internal-links.js';
 export { default as mixin } from './src/mixin.js';
 export * from './src/trailing-slash.js';
+export { default as getUserMixin } from './src/user-model.js';
