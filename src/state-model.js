8ef7c015f64d390035e3ff39ea1cac782347efd3
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-20 17:37:33 +0100

    squash! Add the state model mixin
    make it literate

--- src/state-model.js
+++ src/state-model.js
@@ -1,14 +1,23 @@
 import _ from 'underscore';
 
+// Mixin with the methods needed to turn a regular `Backbone.Model` into a state
+// model.
 var stateMixin = {
+    // By providing a ready `preinitialize` that calls `this.bindStateEvents`,
+    // we can often save the user from having to write any additional code.
     preinitialize: function() {
         this.bindStateEvents();
     },
 
+    // Attach our event handler that emits all the specialized events. This must
+    // be called once during instance creation, in the constructor,
+    // `preinitialize` or `initialize` method.
     bindStateEvents: function() {
         this.on('change', this.broadcastStateEvents);
     },
 
+    // The heart of our mixin. For every changed attribute, trigger appropriate
+    // `'add:'`, `'exit:'`, `'enter:'` and `'remove:'` events.
     broadcastStateEvents: function(model, options) {
         var current = this.attributes,
             previous = this.previousAttributes(),
@@ -28,6 +37,11 @@ var stateMixin = {
     }
 };
 
+// Rather than exporting the raw mixin, we provide a getter function that
+// creates a shallow clone and optionally customizes the methods. If `options`
+// is `true` or omitted, return the mixin without adjustment. If `false`, omit
+// the `preinitialize` method. If `options` is an object, use its properties as
+// overrides.
 function getStateMixin(options) {
     var mixin = stateMixin;
     if (options === false) {
