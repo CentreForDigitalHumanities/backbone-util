18f1dfddad00167bb95c047dd984e9ff95bec177
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-21 17:19:16 +0100

    Make the names of the mixin creation functions more consistent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { default as getAltClickDebugMethods } from './src/click-to-debug.js';
+export { default as getAltClickMixin } from './src/click-to-debug.js';
 export { default as wrapWithCSRF } from './src/csrf.js';
 export * from './src/future-attribute.js';
 export { default as makeLinkEnabler } from './src/internal-links.js';
