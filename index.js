95d52770024c2cbe72f55301014b977aa209691b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-03-21 17:19:55 +0100

    Add the state model mixin to the index

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export { default as wrapWithCSRF } from './src/csrf.js';
 export * from './src/future-attribute.js';
 export { default as makeLinkEnabler } from './src/internal-links.js';
 export { default as mixin } from './src/mixin.js';
+export { default as getStateMixin } from './src/state-model.js';
 export * from './src/trailing-slash.js';
 export { default as getUserMixin } from './src/user-model.js';
